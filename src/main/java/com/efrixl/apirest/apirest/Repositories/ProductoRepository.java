b2754a724ae4fe9eaadcaad47ba0f65a05e269e5
Efrix <zukis5826@Gmail.com>
2025-01-11 00:36:09 -0700

    Hicimos una version configurada de nuestro proyecto APIREST CRUD con POSGRESS para poder desplegar en Railway

---
+++ src/main/java/com/efrixl/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,11 @@
+package com.efrixl.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.efrixl.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository< Producto, Long>{
+
+    
+
+}
